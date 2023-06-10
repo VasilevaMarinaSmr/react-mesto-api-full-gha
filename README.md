423641294778dd476151ee7ad886a54d498c3e33
Marina Vasileva <mv031a@yandex.ru>
2023-06-11 00:24:05 +0400

    Fix: Редактировать readme

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
   
 Пожалуйста, прикрепите в это описание ссылку на сайт, размещенный на Яндекс.Облаке.
 
-Адрес репозитория: https://github.com/...
+Адрес репозитория: https://github.com/VasilevaMarinaSmr/react-mesto-api-full-gha
 
 ## Ссылки на проект
 
-IP-адрес x.x.x.x
+IP-адрес 51.250.27.71
 
-Frontend https://...
+Frontend https://miimesto.nomoredomains.rocks
 
-Backend https://...
+Backend https://api.miimesto.nomoredomains.rocks
--- backend/middlewares/corsconf.js
+++ backend/middlewares/corsconf.js
@@ -7,10 +7,11 @@ const allowedCors = [
 ];
 
 const corsConfig = (req, res, next) => {
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
